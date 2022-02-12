c82c2eac260e7c8bfd610e71ff81795dd5723e6b
Michael-Wang98 <michael.wang1134@gmail.com>
2022-02-12 00:25:26 -0500

    Add bot to r/test and remove sleep due to stream blocking

--- replybot.py
+++ replybot.py
@@ -15,23 +15,22 @@ else:
                             password=os.environ.get("REDDIT_PASSWORD"), 
                             user_agent=os.environ.get("REDDIT_USER_AGENT")) 
 
-subreddit = reddit.subreddit('TCGBotTest937') 
+subreddit = reddit.subreddit('TCGBotTest937+test') 
 
 def reply():
-    sub_comments = subreddit.stream.comments(skip_existing=True)
-    for comment in sub_comments:
-        cards = re.findall("{([^\[\]]*?)}", comment.body)
-        reply = ""
-        for i in set(cards):
-            card_name = [word.capitalize() for word in i.split()]
-            reply += "[" + ' '.join(card_name) + "](https://yugioh.fandom.com/wiki/" + '_'.join(card_name) + ")\n\n"
-        if reply:
-            try:
-                comment.reply(reply)
-            except Exception as err: 
-                print(str(err))
-    return
+    new_comments = subreddit.stream.comments(skip_existing=True)
+    for comment in new_comments:
+            cards = re.findall("{([^\[\]]*?)}", comment.body)
+            reply = ""
+            for i in set(cards):
+                print(i)
+                card_name = [word.capitalize() for word in i.split()]
+                reply += "[" + ' '.join(card_name) + "](https://yugioh.fandom.com/wiki/" + '_'.join(card_name) + ")\n\n"
+            if reply:
+                try:
+                    comment.reply(reply)
+                except Exception as err: 
+                    print(str(err))
+    return 
 
-while True:
-    reply()
-    sleep(20)
+new_ids = reply()
