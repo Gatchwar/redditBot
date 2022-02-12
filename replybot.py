173b9f40b1f2232ad54e12492c803027ebc1e544
Michael-Wang98 <michael.wang1134@gmail.com>
2022-02-12 18:43:14 -0500

    Implement simultaneous submission/comment streams

--- replybot.py
+++ replybot.py
@@ -1,7 +1,6 @@
 import praw  
 import re
 import os
-from time import sleep
 from dotenv import load_dotenv  # import dotenv to get environment variables from .env file
 load_dotenv()
 
@@ -17,10 +16,32 @@ else:
 
 subreddit = reddit.subreddit('TCGBotTest937+test') 
 
-def reply():
-    new_comments = subreddit.stream.comments(skip_existing=True)
-    for comment in new_comments:
+def main():
+    # subreddit.stream yields new comments/submissions as they are created
+    # skip_existing=True ignores all comments/submissions created prior to the stream's creation
+    # pause_after=-1 stream yields None after each request (allows monitoring 2 streams without blocking each other) 
+    new_comments = subreddit.stream.comments(skip_existing=True, pause_after=-1)
+    new_submissions = subreddit.stream.submissions(skip_existing=True, pause_after=-1)
+    while True:
+        for comment in new_comments:
+            if comment is None:
+                break
             cards = re.findall("{([^\[\]]*?)}", comment.body)
+            reply = ""
+            for i in set(cards):  # cast cards to set to remove duplicates
+                print(i)
+                card_name = [word.capitalize() for word in i.split()]
+                reply += "[" + ' '.join(card_name) + "](https://yugioh.fandom.com/wiki/" + '_'.join(card_name) + ")\n\n"
+            if reply:  # only reply when any card names are found
+                try:
+                    comment.reply(reply)
+                except Exception as err: 
+                    print(str(err))
+    
+        for submission in new_submissions:
+            if submission is None:
+                break
+            cards = re.findall("{([^\[\]]*?)}", submission.selftext)
             reply = ""
             for i in set(cards):
                 print(i)
@@ -28,9 +49,11 @@ def reply():
                 reply += "[" + ' '.join(card_name) + "](https://yugioh.fandom.com/wiki/" + '_'.join(card_name) + ")\n\n"
             if reply:
                 try:
-                    comment.reply(reply)
+                    submission.reply(reply)
                 except Exception as err: 
                     print(str(err))
-    return 
 
-new_ids = reply()
+
+if __name__ == "__main__":
+    main()
+
