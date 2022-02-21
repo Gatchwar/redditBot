ffc34aa36c421443ed0f56f898b164a5fc4e8392
Michael-Wang98 <michael.wang1134@gmail.com>
2022-02-21 00:22:44 -0500

    Comments in double curly braces prints card info

--- replybot.py
+++ replybot.py
@@ -7,8 +7,9 @@ from difflib import get_close_matches
 from dotenv import load_dotenv  # import dotenv to get environment variables from .env file
 load_dotenv()
 
-REGEX = '{([^\[\]]*?)}'
-ABOUT_BOT = '^^{CARDNAME} ^^to ^^invoke ^^a ^^card'
+REGEX = '(?<=(?<!\{)\{)([^{}]*)(?=\}(?!\}))'
+LONG_REGEX = '{{([^\[\]]*?)}}'
+ABOUT_BOT = '^^{CARDNAME} ^^to ^^invoke ^^a ^^card, ^^{{CARDNAME}} ^^to ^^also ^^get ^^information'
 BASE_URL = 'http://yugiohprices.com/'
 WIKI_URL = 'https://yugioh.fandom.com/wiki/'
 MASTER_DUEL_URL = 'https://www.masterduelmeta.com/cards/'
@@ -26,21 +27,62 @@ subreddit = reddit.subreddit(os.environ.get("REDDIT_SUBREDDITS")) # Fetch subred
 req = requests.get(BASE_URL + 'api/card_names')
 card_names_list = json.loads(json.dumps(req.json())) # get every card name and put into a list
 
+def format_card_data(card):
+    try:
+        try:
+            req = requests.get(BASE_URL + 'api/card_data/' + card.replace(' ', '+'))
+        except Exception as e:
+            print(e)
+            return None
+        else:
+            req.connection.close()
+            if req.ok:
+                json = req.json()
+                text = ''
+                if json.get('status', '') == 'success':
+                    data = json['data']
+                    if data['card_type'] == 'monster':
+                        types = data['type'].replace(' ', '').split('/')
+                        text += '{} Monster\n\n{} / {}\n\n{}\n\nAtk: {}'.format(types[1], data['family'].capitalize(), types[0], data['text'].replace('\n\n', ''), data['atk'])
+                    else:
+                        text += '{} {}\n\n{}'.format(data['property'], data['card_type'].capitalize(), data['text'].replace('\n\n', ' '))
+                return text 
+
+    except Exception as e:
+        print(e)
+        return None
+
 def reply_builder(cards):
     reply = ''
-    for i in cards:  # cast cards to set to remove duplicates
+    for i in cards:
         print(i)
         card_input = ' '.join([word.capitalize() for word in i.split()]) # capitalize first letter of each word to make matching easier
         closest = get_close_matches(card_input, card_names_list, 1, 0.7) # finds card with name closest to input name if exists
         if closest:
             card_name = closest[0]
-            reply += "[" + ' '.join(card_name) + "](" + BASE_URL + "api/card_image/" + card_name.replace(' ', '+') + ")"
+            reply += "[" + card_name + "](" + BASE_URL + "api/card_image/" + card_name.replace(' ', '+') + ")"
             reply += ' - '
             reply += "[(wiki)](" + WIKI_URL + card_name.replace(' ', '_') + ") "
             reply += "[($)](" + BASE_URL + "card_price?name=" + card_name.replace(' ', '+').replace('&', '%26') + ") "
             reply += "[(MD)](" + MASTER_DUEL_URL + closest[0] + ")"
             reply += '\n\n'
     return reply
+
+def long_reply_builder(cards):
+    reply = ''
+    for i in cards:
+            print(i)
+            card_input = ' '.join([word.capitalize() for word in i.split()]) # capitalize first letter of each word to make matching easier
+            closest = get_close_matches(card_input, card_names_list, 1, 0.7) # finds card with name closest to input name if exists
+            if closest:
+                card_name = closest[0]
+                reply += "[" + card_name + "](" + BASE_URL + "api/card_image/" + card_name.replace(' ', '+') + ")\n\n"
+                reply += format_card_data(card_name) + '\n\n'
+                reply += "[(wiki)](" + WIKI_URL + card_name.replace(' ', '_') + ") "
+                reply += "[($)](" + BASE_URL + "card_price?name=" + card_name.replace(' ', '+').replace('&', '%26') + ") "
+                reply += "[(MD)](" + MASTER_DUEL_URL + closest[0] + ")"
+                reply += '\n\n'
+    return reply 
 
 def main():
     # subreddit.stream yields new comments/submissions as they are created
@@ -57,8 +99,11 @@ def main():
                 break
             if comment is None or author == username:  # do not self-reply
                 break
-            cards = re.findall(REGEX, comment.body)  # Regex to find all instances of {CARDNAME}
-            reply = reply_builder(set(cards))
+            cards = re.findall(REGEX, comment.body)  # Find all instances of {CARDNAME}
+            long_cards = re.findall(LONG_REGEX, comment.body)  # Find all instances of {{CARDNAME}}
+            reply = ''
+            reply += reply_builder(set(cards))
+            reply += long_reply_builder(set(long_cards))
             if reply:  # only reply when any card names are found
                 try:
                     reply += ABOUT_BOT
@@ -70,7 +115,9 @@ def main():
             if submission is None:
                 break
             cards = re.findall(REGEX, submission.selftext)
+            long_cards = re.findall(LONG_REGEX, submission.selftext)  # Find all instances of {{CARDNAME}}
             reply = reply_builder(set(cards))
+            reply += long_reply_builder(set(long_cards))
             if reply:
                 try:
                     reply += ABOUT_BOT
